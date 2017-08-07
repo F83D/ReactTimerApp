0a40bdba053fbbedee640eee04f1ef688376c09d
Frederic <frederic.fayet@gmail.com>
2017-08-07 19:00:53 +0200

    Add countdown and CSS for the clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
     render: function() {
         return (
             <div>
                 <h3>Page COUNTDOWN</h3>
+                <Clock totalSeconds={129}/>
             </div>
         );
     }
